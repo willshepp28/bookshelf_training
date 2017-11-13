3facecc27f5f59874c28dcb89ae60a20d79ff0f3
willshepp28 <willsheppard28@outlook.com>
2017-11-12 21:50:06 -0500

    included /todo post route

--- routes/todo.js
+++ routes/todo.js
@@ -1,18 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 // bring database connection in
 const knex = require('../db/knex');
 
-/* This router is mounted on http://localhost:3000/todo  */
-router.get('/', function(req, res, next) {
 
-// Get all todos from the database
-  knex('todo')
-    .select()
-    .then(todos => {
-        res.render('all', { todos: todos });
+function validTodo(todo) {
+    return typeof todo.title == 'string' && 
+        todo.title.trim() != '' && 
+        typeof todo.priority == 'number';
+}
+
+/* This router is mounted on http://localhost:3000/todo  */
+router.route('/')
+    .get((req, res) => {
+
+        // Get all todos from the database
+        knex('todo')
+            .select()
+            .then(todos => {
+                res.render('all', { todos: todos });
+            })
     })
+    .post((req, res) => {
+        console.log(req.body);
+        if (validTodo(req.body)) {
+
+            const todo = {
+                title: req.body.title,
+                description: req.body.description,
+                priority: req.body.priority
+            };
+
+            // insert into database
+            knex('todo')
+                .insert(todo, 'id')
+                .then(todo => {
+                    const id = ids[0];
+                    res.redirect(`/todo/${id}`);
+                })
+        } else {
+
+            // respond with an error
+            res.status(500);
+            res.render('error', { message: 'Invalid todo'});
+
+        }
+    })
+
+
+
+
+
+router.get('/new', (req, res) => {
+    res.render('new');
 });
+
+
 
 module.exports = router;
